438bc1caf0f30d6c8856d5209f92dc02cbaddc00
Marpond <sonkoly.marci02@gmail.com>
2023-05-24 20:35:17 +0200

    changed jenkins to use playwright

--- PlaywrightTests/Tests.cs
+++ PlaywrightTests/Tests.cs
@@ -30,7 +30,7 @@ public class Tests
     [SetUp]
     public async Task SetUp()
     {
-        await _page.GotoAsync("http://128.140.9.68:81");
+        await _page.GotoAsync(Environment.GetEnvironmentVariable("URL")!);
     }
 
     [Test]
